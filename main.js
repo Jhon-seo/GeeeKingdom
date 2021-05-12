34bc8eb5cfbc55bf88083df86303259aaaba62d7
Yen Colon <yendejesusc41@gmail.com>
2021-05-11 23:35:19 -0400

    commit2

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var equis = document.getElementById('botonEquis');
 
 var menuContenido = document.getElementById('menuDeContenido');
 
+var comics = document.querySelectorAll('.contenedorDeComic');
+
 menu.addEventListener("click",mostrarMenu);
 
 equis.addEventListener("click",ocultarMenu);
@@ -27,12 +29,53 @@ function ocultarMenu() {
   equis.classList.remove('botonMuestraMenu');
   equis.classList.add('botonOcultaMenu');
 
-menuContenido.classList.add("Menu_horizontal_no_desplegado");
-menuContenido.classList.remove("Menu_horizontal_desplegado");
+  menuContenido.classList.add("Menu_horizontal_no_desplegado");
+  menuContenido.classList.remove("Menu_horizontal_desplegado");
 
   menu.classList.remove('botonOcultaMenu');
   menu.classList.add('botonMuestramenu');
 
 }
 
+function mostrarOpciconesDelComic(identificador) {
+  var botonDeOpciones = document.getElementById(identificador);
+  botonDeOpciones.classList.remove('noMostrarOpciones');
+  botonDeOpciones.classList.add('mostrarOpciones');
+  
+  botonDeOpciones.addEventListener('click', opcionesDeComic)
+
+}
+
+function noMostrarOpciconesDelComic(identificador) {
+  var botonDeOpciones = document.getElementById(identificador);
+  botonDeOpciones.classList.remove('mostrarOpciones');
+  botonDeOpciones.classList.add('noMostrarOpciones');
+  
+}
+
+function  opcionesDeComic () {
+  var opciones = document.getElementById('opciones');
+  opciones.classList.remove('noMostrarOpciones');
+  var ocultarOpciones = document.getElementById('ocultarOpciones');
+
+  ocultarOpciones.addEventListener('click', function () {
+    opciones.classList.add('noMostrarOpciones');
+  })
+}
+window.onload = function(){
+
+for (let i= 0; i < comics.length; i++) {
+
+  comics[i].addEventListener("mouseover", function() {
+    mostrarOpciconesDelComic(i)
+  } );
+
+  comics[i].addEventListener("mouseout",function(){
+     noMostrarOpciconesDelComic(i)});
+
+}
+
+
+
+}
 
